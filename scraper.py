7513ef4e2eec19c8401e260d5d79ba96f3a28fdd
ianfernandes <jonazkue.f@gmail.com>
2017-03-27 22:39:38 +0200

    scraper working

--- scraper.py
+++ scraper.py
@@ -1,3 +1,44 @@
-from settings import INITIAL_URL, driver
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import json
+import time
+from random import randint
+from bs4 import BeautifulSoup
+from settings import INITIAL_URL, DRIVER
+from selenium.webdriver.common.keys import Keys
 
-driver.get(INITIAL_URL)
+
+def wait():
+    random_number = randint(2, 5)
+    time.sleep(random_number)
+
+
+def get_countries_from_json():
+    with open('countries.json') as countries_file:
+        data = json.load(countries_file)
+        return data['Countries']
+
+DRIVER.get(INITIAL_URL)
+assert "International" in DRIVER.title
+inputElement = DRIVER.find_element_by_id("countryName")
+
+
+def get_calling_landlines(content):
+    soup = BeautifulSoup(content, "html.parser")
+    land_line = soup.find("td", {"id": "landLine"})
+    if land_line:
+        return land_line.strong.text
+
+
+for country in get_countries_from_json():
+    inputElement.clear()
+    inputElement.send_keys(country)
+    inputElement.send_keys(Keys.ENTER)
+    wait()
+    DRIVER.find_element_by_id("paymonthly").click()
+    print get_calling_landlines(DRIVER.page_source)
+    wait()
+
+
+
+
--- settings.py
+++ settings.py
@@ -2,4 +2,4 @@ from selenium import webdriver
 
 INITIAL_URL = "http://international.o2.co.uk/internationaltariffs/calling_abroad_from_uk"
 
-driver = webdriver.Firefox()
+DRIVER = webdriver.Firefox()
